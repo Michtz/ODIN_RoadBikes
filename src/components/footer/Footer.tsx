fd19d9468cf0ac72b3e76a2f9cd8dcefc8e49259
Michael Venetz <105941997+Michtz@users.noreply.github.com>
2026-04-01 19:13:09 +0200

    added address

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,29 +4,102 @@ import Link from '@/components/system/link/Link';
 import MaterialIcon from '@/components/system/materialIcon/MaterialIcon';
 import MapFacade from '@/components/system/mapFacade/MapFacade';
 
+const localBusinessJsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'LocalBusiness',
+  name: 'OdinBikes',
+  email: 'info@odinbikes.ch',
+  telephone: '+41340704',
+  contactPoint: {
+    '@type': 'ContactPoint',
+    telephone: '+41340704',
+    contactType: 'customer service',
+  },
+  employee: {
+    '@type': 'Person',
+    name: 'Maikel Blommerde',
+  },
+  address: {
+    '@type': 'PostalAddress',
+    streetAddress: 'Gemeindehausplatz 13',
+    postalCode: '6048',
+    addressLocality: 'Horw',
+    addressCountry: 'CH',
+  },
+};
+
 const Footer: FC = () => {
   return (
     <footer className={style.footer}>
+      <script
+        type="application/ld+json"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(localBusinessJsonLd),
+        }}
+      />
+
       <div className={style.footerContainer}>
         <div className={style.footerContent}>
           <div className={style.footerColumn}>
             <h3 className={style.columnTitle}>Kontakt</h3>
-            <ul className={style.linkList}>
-              <li className={style.contactListItem}>
-                <MaterialIcon icon="mail" iconSize="small" />
-                <span className={style.contactContent}>
-                  Email:&nbsp;
-                  <a href="mailto:info@odinbikes.ch">info@odinbikes.ch</a>
-                </span>
-              </li>
-              <li className={style.contactListItem}>
-                <MaterialIcon icon="phone" iconSize="small" />
-                <span className={style.contactContent}>
-                  Telefon:&nbsp;
-                  <a href="tel:+0413407044">+041 340 70 44</a>
-                </span>
-              </li>
-            </ul>
+
+            {/* Semantic address block using <address> element */}
+            <address className={style.addressBlock}>
+              <ul className={style.linkList}>
+                <li className={style.contactListItem}>
+                  <MaterialIcon
+                    icon="mail"
+                    iconSize="small"
+                    aria-hidden="true"
+                  />
+                  <span className={style.contactContent}>
+                    <a
+                      href="mailto:info@odinbikes.ch"
+                      rel="noopener noreferrer"
+                    >
+                      info@odinbikes.ch
+                    </a>
+                  </span>
+                </li>
+                <li className={style.contactListItem}>
+                  <MaterialIcon
+                    icon="phone"
+                    iconSize="small"
+                    aria-hidden="true"
+                  />
+                  <span className={style.contactContent}>
+                    <a href="tel:+41340704">+041 340 70 44</a>
+                  </span>
+                </li>
+                <li className={style.contactListItem}>
+                  <MaterialIcon
+                    icon="person"
+                    iconSize="small"
+                    aria-hidden="true"
+                  />
+                  <span className={style.contactContent}>
+                    <span itemProp="name">Maikel Blommerde</span>
+                  </span>
+                </li>
+                <li className={style.contactListItem}>
+                  <MaterialIcon
+                    icon="location_on"
+                    iconSize="small"
+                    aria-hidden="true"
+                  />
+                  <span className={style.contactContent}>
+                    <span itemScope itemType="https://schema.org/PostalAddress">
+                      <span itemProp="streetAddress">Gemeindehausplatz 13</span>
+                      ,
+                      <br />
+                      <span itemProp="postalCode">6048</span>
+                      <span itemProp="addressLocality">Horw</span>
+                    </span>
+                  </span>
+                </li>
+              </ul>
+            </address>
           </div>
 
           <div className={style.mapContainer}>
@@ -35,13 +108,16 @@ const Footer: FC = () => {
         </div>
 
         <div
-          id={'contact'}
+          id="contact"
           className={`${style.footerColumn} ${style.additionalInfo}`}
         >
-          <Link href="/impressum" aria-label="Das Rechtliche">
+          <Link href="/impressum" aria-label="Impressum – Das Rechtliche">
             Impressum
           </Link>
-          <Link href="/legal" aria-label="Datenschutz">
+          <Link
+            href="/legal"
+            aria-label="Datenschutz – Unsere Datenschutzerklärung"
+          >
             Datenschutz
           </Link>
         </div>
